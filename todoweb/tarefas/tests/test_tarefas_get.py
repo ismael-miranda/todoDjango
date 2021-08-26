6cf54969f982c6303687081a001491dd22eb391c
laptop <ismael@ismael.com>
2021-08-25 22:05:58 -0300

    Add a listagem das tarefas criadas

--- todoweb/tarefas/tests/test_tarefas_get.py
+++ todoweb/tarefas/tests/test_tarefas_get.py
@@ -2,10 +2,11 @@
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
+from todoweb.tarefas.models import Tarefa
 
 
 @pytest.fixture
-def resposta(client):
+def resposta(client, db):
     resp = client.get(reverse('tarefas:home'))
     return resp
 
@@ -20,3 +21,24 @@ def test_form_available(resposta):
 
 def test_btn_salvar_available(resposta):
     assertContains(resposta, '<button type="submit"')
+
+
+@pytest.fixture
+def lista_tarefas_pendentes(db):
+    tarefas = [
+        Tarefa(nome='Tarefa 1', feita=False),
+        Tarefa(nome='Tarefa 2', feita=False),
+    ]
+    Tarefa.objects.bulk_create(tarefas)
+    return tarefas
+
+
+@pytest.fixture
+def resp_lista_tarefas(client, lista_tarefas_pendentes):
+    resp = client.get(reverse('tarefas:home'))
+    return resp
+
+
+def test_lista_tarefas_pendents_present(resp_lista_tarefas, lista_tarefas_pendentes):
+    for tarefa in lista_tarefas_pendentes:
+        assertContains(resp_lista_tarefas, tarefa.nome)
